8bc00365bf1c5e7279f1ca99290a94fb11268a4c
aznoqmous <paul.landreau931v@gmail.com>
2026-03-27 11:32:13 +0100

    added isle 3 puzzle logic and particles

--- scripts/puzzle_area.gd
+++ scripts/puzzle_area.gd
@@ -1,11 +1,16 @@
 extends Node3D
 @export var boat: Boat
 @export var area_3d: Area3D
+@export var explosion_particles: GPUParticles3D
+@export var disabled_object: Node3D
 
 func _ready():
 	boat.set_visible(false)
 	boat.area_3d.monitoring = false
 	area_3d.area_entered.connect(func(area):
-		boat.set_visible(true)
-		boat.area_3d.monitoring = true
+		if disabled_object: disabled_object.set_visible(false)
+		if boat:
+			boat.set_visible(true)
+			boat.area_3d.monitoring = true
+		if explosion_particles: explosion_particles.emitting = true
 	)
